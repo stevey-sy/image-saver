8e6b333fd378121ddf36770acae0d1fd78d2fad2
sinsy8989 <sinsy89@monkey-soft.com>
2025-06-30 17:49:18 +0900

    snackBarManager 리펙토링

--- app/src/main/java/com/sy/imagesaver/presentation/manager/SnackBarManager.kt
+++ app/src/main/java/com/sy/imagesaver/presentation/manager/SnackBarManager.kt
@@ -1,49 +1,31 @@
 package com.sy.imagesaver.presentation.manager
 
-import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
-import androidx.compose.ui.graphics.Color
-import com.sy.imagesaver.presentation.theme.Orange
-import com.sy.imagesaver.presentation.theme.Purple
-import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class SnackBarManager @Inject constructor() {
+    private val _snackBarFlow = MutableSharedFlow<SnackBarEvent>()
+    val snackBarFlow: SharedFlow<SnackBarEvent> = _snackBarFlow.asSharedFlow()
     
-    private var snackbarHostState: SnackbarHostState? = null
-    private var coroutineScope: CoroutineScope? = null
-    private var showSnackbarWithColor: ((String, Color) -> Unit)? = null
-    
-    fun setSnackbarHostState(hostState: SnackbarHostState, scope: CoroutineScope) {
-        this.snackbarHostState = hostState
-        this.coroutineScope = scope
+    suspend fun showSuccessSnackbar(message: String) {
+        _snackBarFlow.emit(SnackBarEvent.Success(message))
     }
     
-    fun setShowSnackbarWithColor(callback: (String, Color) -> Unit) {
-        this.showSnackbarWithColor = callback
+    suspend fun showErrorSnackbar(message: String) {
+        _snackBarFlow.emit(SnackBarEvent.Error(message))
     }
     
-    fun showErrorSnackbar(message: String, duration: SnackbarDuration = SnackbarDuration.Long) {
-        showSnackbarWithColor?.invoke(message, Purple)
-    }
-    
-    fun showSuccessSnackbar(message: String, duration: SnackbarDuration = SnackbarDuration.Short) {
-        showSnackbarWithColor?.invoke(message, Orange) // 주황색
-    }
-    
-    enum class SnackbarDuration {
-        Short, Long, Indefinite
-    }
-    
-    private fun SnackbarDuration.toSnackbarDuration(): androidx.compose.material3.SnackbarDuration {
-        return when (this) {
-            SnackbarDuration.Short -> androidx.compose.material3.SnackbarDuration.Short
-            SnackbarDuration.Long -> androidx.compose.material3.SnackbarDuration.Long
-            SnackbarDuration.Indefinite -> androidx.compose.material3.SnackbarDuration.Indefinite
-        }
+    sealed class SnackBarEvent {
+        abstract val message: String
+        
+        data class Success(override val message: String) : SnackBarEvent()
+        data class Error(override val message: String) : SnackBarEvent()
     }
 }
 
